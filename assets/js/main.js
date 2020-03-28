cd1b070f4d1ac7e41f1a8d8670c621c8ee66a22f
igna2019 <losmacocos@gmail.com>
2020-03-28 20:12:44 +0000

    Web Site completed-Pending Readme

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function toggleNavigation() {
+  let link = document.getElementById("myLinks");
+  if (link.style.display === "block") {
+    link.style.display = "none";
+  } else {
+    link.style.display = "block";
+  }
+}
